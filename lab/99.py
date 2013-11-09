91d3d00fe35847250eac26d002b903d7e6a94f85
Beyoung <twainyoung@gmail.com>
2013-11-09 21:38:25 +0800

    Create 99.py
    99乘法表

---
+++ lab/99.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+'''
+Created on 2013-11-9
+
+@author: young
+'''
+def V1_99(start,end):    
+    seq=xrange(start,end+1)
+    magicstring=''
+    for i in seq:
+        row=''
+        for j in xrange(i):
+            row=row+'%s*%s=%s\t'%(i,j+1,i*(j+1))        
+        magicstring+=row+'\n'
+    return magicstring
+
+
+if __name__=='__main__':
+    print V1_99(1,9
